ca243a5565173074ef23680e62095d90838fb98c
imim <im.imanmaliki@gmail.com>
2021-09-08 17:04:29 +0800

    category component edited

--- src/components/CategoryTab.js
+++ src/components/CategoryTab.js
@@ -5,8 +5,8 @@ import CategoryPage from './CategoryPage';
 const CategoryTab = ({ match }) => {
     return (
         <div>
-            <Category />
-            <CategoryPage match={match} />
+            <div className="bg-gray-600 w-full h-full pt-10 pb-10 bg-cover bg-fixed" style={{backgroundImage:`url('https://i.ibb.co/Zgj3KQh/sign-up-news-letter-banner.jpg')`}}><Category /></div>
+            <div className="pb-10"><CategoryPage match={match} /></div>
         </div>
     )
 }
